9a868abd2a02d35a86ebd3877550f76c43e84139
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-05-04 20:43:42 -0600

    configure ability to prompt katas and store results

---
+++ BoxesTemplate.js
@@ -0,0 +1,7 @@
+const {Topics} = require("./Topics")
+
+const Box1 = Topics.map(each => each.name)
+
+const Box2 = []
+
+module.exports = {Box1, Box2}
---
+++ Topics.js
@@ -0,0 +1,8 @@
+
+const DesignPatterns = require("./DesignPatterns");
+const Elixir = require("./Elixir");
+const Topics = [...DesignPatterns, ...Elixir];
+
+module.exports = {
+    Topics
+}
