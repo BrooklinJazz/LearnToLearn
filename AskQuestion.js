10b6ea714feb378182e84c217335aaa0087a8dd0
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-05-04 21:04:20 -0600

    Pose topics as questions with descriptions

--- AskQuestion.js
+++ AskQuestion.js
@@ -1,30 +1,30 @@
 const fs = require("fs");
 const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
 const {Topics} = require("./Topics")
 const { Box1, Box2 } = require("./Boxes");
 
 function ask (question) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
   return new Promise(resolve => {
     rl.question(question, resolve);
   });
 }
 
-const answerQuestion = async (questionName, box1, box2) => {
-  let newBox1 = [...box1]
-  let newBox2 = [...box2]
-  return ask("Did you complete the kata? Y/N").then(async answer => {
+const sortBoxes = async (questionName) => {
+  let box1 = [...Box1]
+  let box2 = [...Box2]
+  return ask("Did you answer correctly? Y/N: ").then(async answer => {
       switch (answer) {
         case "Y":
         case "y":
         case "yes":
         case "Yes":
-          newBox2.push(questionName)
-          newBox1 = newBox1.filter(each => each !== questionName)
+          box2.push(questionName)
+          box1 = box1.filter(each => each !== questionName)
           break;
         case "N":
         case "No":
@@ -36,22 +36,20 @@ const answerQuestion = async (questionName, box1, box2) => {
           console.warn("INVALID INPUT")
           return await answerQuestion(questionName, box1, box2);
       }
-      rl.close();
-      return [newBox1, newBox2];
+      return [box1, box2];
     });
 };
 
-const askQuestion = async () => {
-  if (Box1.length === 0) {
-    console.warn("You're first box is empty! rewrite me!");
-    return
-  }
-  // select question
-  const question = Box1[0];
-  const topic = Topics.find(each => each.name === question);
-  // ask for kata completion
-  console.log(`Answer this kata about ${topic.name}: ${topic.katas[0]} huh`);
-  const boxes = await answerQuestion(topic.name, Box1, Box2);
+const describe = async (topic) => {
+    return ask(`Please describe ${topic.name}:\n`).then(async answer => {
+      console.log("You said:\n\n")
+      console.warn(answer)
+      console.log("\n\n We expected:\n\n")
+      console.log(topic.description)
+    });
+}
+
+const saveBoxes = async (boxes) => {
   const [box1, box2] = boxes.reduce((total, each) => {
     return [...total, each.map(each => `'${each}'`)]
   }, []);
@@ -68,6 +66,20 @@ module.exports = {Box1, Box2}
       console.log("Your Boxes have been updated");
     }
   );
+}
+
+const describeConcept = async () => {
+  if (Box1.length === 0) {
+    console.warn("You're first box is empty! rewrite me!");
+    return
+  }
+  // select question
+  const question = Box1[0];
+  const topic = Topics.find(each => each.name === question);
+
+  await describe(topic)
+  const boxes = await sortBoxes(topic);
+  await saveBoxes(boxes)
 };
 
-askQuestion();
+describeConcept();
--- Boxes.js
+++ Boxes.js
@@ -1,7 +1,6 @@
-const {Topics} = require("./Topics")
 
-const Box1 = Topics.map(each => each.name)
-
+const Box1 = ['Factory Method','Abstract Factory','Adapter','Bridge','Chain of Responsibility','Command','Iterator','Mediator','Builder','Prototype','Composite','Decorator','Memento','Observer','State','Strategy','Singleton','Facade','FlyWeight','TemplateMethod','Visitor','Proxy','Basic Types And Operations','Pattern Matching','Control Flow','Recursion','Enumerables','Processes','Structs','Protocols','Concurrency']
 const Box2 = []
 
 module.exports = {Box1, Box2}
+    
