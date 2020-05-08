02c743a2d44e88589ec6de53f6f409c765c0131d
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-05-07 22:46:59 -0600

    refute deck == shuffle deck

--- Elixir/cards/test/cards_test.exs
+++ Elixir/cards/test/cards_test.exs
@@ -2,7 +2,13 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
+ test "create_deck _ 20 cards expected" do
+   deck_length = length(Cards.create_deck)
+   assert(deck_length) == 20
+ end
+
+ test "shuffle _ randomizes deck" do
+  deck = Cards.create_deck
+  refute deck == Cards.shuffle(deck)
+ end
 end
