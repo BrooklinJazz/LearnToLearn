caf793fd8268b8511a96492f79b0bf6c933333e3
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-05-10 22:40:45 -0600

    docs for hash_input

--- Elixir/identicons/lib/identicons.ex
+++ Elixir/identicons/lib/identicons.ex
@@ -11,12 +11,17 @@ defmodule Identicons do
   @doc """
   hash_input
 
-  takes in a string and returns a sequence of charcodes
+  takes in a string and returns the same sequence of numbers for the specific string
 
   # Examples
 
   iex> Identicons.hash_input("banana")
   [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
+
+  iex> Identicons.hash_input("orange")
+  [254, 1, 214, 122, 0, 45, 250, 15, 58, 192, 132, 41, 129, 66, 236, 205]
+  iex> Identicons.hash_input("orange")
+  [254, 1, 214, 122, 0, 45, 250, 15, 58, 192, 132, 41, 129, 66, 236, 205]
   """
   def hash_input(input) do
     :crypto.hash(:md5, input)
