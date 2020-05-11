a72c9adc25c561f922c0f215ec1ea0c33e180083
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-05-10 22:39:21 -0600

    identicons configuring

--- Elixir/identicons/lib/identicons.ex
+++ Elixir/identicons/lib/identicons.ex
@@ -3,16 +3,23 @@ defmodule Identicons do
   Documentation for `Identicons`.
   """
 
+  def main(input) do
+    input
+    |> hash_input
+  end
+
   @doc """
-  Hello world.
+  hash_input
 
-  ## Examples
+  takes in a string and returns a sequence of charcodes
 
-      iex> Identicons.hello()
-      :world
+  # Examples
 
+  iex> Identicons.hash_input("banana")
+  [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
   """
-  def hello do
-    :world
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list()
   end
 end
--- Elixir/identicons/test/identicons_test.exs
+++ Elixir/identicons/test/identicons_test.exs
@@ -1,8 +1,4 @@
 defmodule IdenticonsTest do
   use ExUnit.Case
   doctest Identicons
-
-  test "greets the world" do
-    assert Identicons.hello() == :world
-  end
 end
