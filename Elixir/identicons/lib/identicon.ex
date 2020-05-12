a5e16c304717aafd95932ad9c5268fdc7a50e968
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-05-11 20:24:51 -0600

    Identicon Pixel Map

--- Elixir/identicons/lib/identicon.ex
+++ Elixir/identicons/lib/identicon.ex
@@ -8,6 +8,9 @@ defmodule Identicon do
     |> hash_input
     |> hash_to_image
     |> pick_color
+    |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
   end
 
   @doc """
@@ -19,19 +22,74 @@ defmodule Identicon do
 
   iex> Identicon.hash_input("banana")
   [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
+
   """
   def hash_input(input) do
-    hex = :crypto.hash(:md5, input)
+    :crypto.hash(:md5, input)
     |> :binary.bin_to_list()
   end
 
+  @doc """
+  takes a hash and returns an Identicon with hex
+
+  # Examples
+  iex> Identicon.hash_to_image([114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65])
+  %Identicon.Image{
+  hex: [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
+  }
+  """
   def hash_to_image(hex) do
     %Identicon.Image{hex: hex}
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
-    [r,g, b]
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  @doc """
+  mirror row takes in a list of 3 elements and mirrors them
+
+  # Examples
+
+  iex> Identicon.mirror_row([1,2,3])
+  [1,2,3,2,1]
+  """
+  def mirror_row(row) do
+    [first, second, _tail] = row
+    row ++ [second, first]
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid =
+      Enum.filter(grid, fn {code, _index} ->
+        rem(code, 2) == 0
+      end)
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map(grid, fn {_code, index} ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+      {top_left, bottom_right}
+    end)
+
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  
 end
